ae2d9817e83117601224e6415d66041d19e74c2b
lkdev <lucas.kano@outlook.com>
2022-05-07 19:33:09 -0300

    Ddesen volvimento upload leitura csv

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import UploadFileForm
 import csv, io
+from datetime import datetime
 
 def index(request):
     return render(request, 'index.html')
@@ -11,11 +12,29 @@ def upload_file(request):
     if request.method == 'POST' and  request.FILES['file']:
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            handle_uploaded_file(request.FILES['file'])
+            _handle_uploaded_file(request.FILES['file'])
         return render(request, 'index.html')
 
-def handle_uploaded_file(f):
+def _handle_uploaded_file(f):
     dataset = f.read().decode('UTF-8')
     io_string = io.StringIO(dataset)
+    count = 0
+    data = None
     for row in csv.reader(io_string, delimiter=','):
-        print(row)
+        # print(row)
+        if count == 0:
+            data = datetime.strptime(row[7], '%Y-%m-%dT%H:%M:%S')
+            print('Primeira linha:', data)
+        else:
+            
+            datetimex = datetime.strptime(row[7], '%Y-%m-%dT%H:%M:%S')
+
+            print('Data do arquivo:', data)
+            print('Data da linha', count,':', datetimex)
+
+            if not data.day ==  datetimex.day or not data.month ==  datetimex.month or not data.year ==  datetimex.year :
+                print('IGNORAR')
+        count += 1
+            
+        #print(row)
+
