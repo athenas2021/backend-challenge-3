fcc9bcb4a301fdaef83441d686ad189a4d254f9f
lkdev <lucas.kano@outlook.com>
2022-05-08 20:43:25 -0300

    user creation·

--- users/views.py
+++ users/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from .forms import UserCreationForm
+from .models import UserManager, User
 
-# Create your views here.
+
+def users(request):
+    all_users = list(User.objects.all().values())
+    data = {
+        'all_users': all_users
+    }
+    return render(request, 'users.html', data)
+
+
+def create_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            print('valido')
+        return render(request, 'users.html')
+
+def user(request):
+    return render(request, 'user.html')
+
+
+'''
+- Apenas 2 informações serão necessárias no cadastro: Nome e Email, sendo ambas obrigatórias
+- A aplicação deve gerar uma senha aleatória para o usuário, composta de 6 números;
+- A senha deverá ser enviada para o email do usuário sendo cadastrado;
+- A senha não deve ser armazenada no banco de dados em texto aberto, devendo ser salvo um hash dela, 
+gerado pelo algoritmo BCrypt;
+- A aplicação não deve permitir o cadastro de um usuário com o email de outro usuário já cadastrado, 
+devendo exibir uma mensagem de erro caso essa situação ocorra;
+- A aplicação deve ter um usuário padrão previamente cadastrado, com nome: Admin, email: admin@email.com.br e senha: 123999;
+- O usuário padrão não pode ser editado e nem excluído da aplicação, tampouco deve ser exibido na lista de usuários cadastrados;
+- Qualquer usuário tem permissão para listar, cadastrar, alterar e excluir outros usuários;
+- Um usuário não pode excluir ele próprio da aplicação.
+'''
