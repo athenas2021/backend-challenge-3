d4c16d7f20712b8bd7343321956517f9da3c0037
lkdev <lucas.kano@outlook.com>
2022-05-08 08:57:09 -0300

    models creation: DayProcess, Transaction

--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Transaction(models.Model):
+    origin_bank = models.CharField(blank=False, null=False, max_length=100)
+    origin_agency = models.CharField(blank=False, null=False, max_length=100)
+    origin_account = models.CharField(blank=False, null=False, max_length=100)
+    destiny_bank = models.CharField(blank=False, null=False, max_length=100)
+    destiny_agency = models.CharField(blank=False, null=False, max_length=100)
+    destiny_account = models.CharField(blank=False, null=False, max_length=100)
+    transaction_value = models.DecimalField(blank=False, null=False, decimal_places=2, max_digits=20)
+    transaction_date_time = models.DateField(auto_now=False, blank=False, null=False)
+
+    db_table = 'transaction'
+
+
+class DayProcess(models.Model):
+    date = models.DateField(auto_now=False, blank=False, null=False)
+    transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
+
+    db_table = 'day_process'
+
