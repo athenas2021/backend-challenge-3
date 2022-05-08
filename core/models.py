195b25813582f815a3e1f83ec9b06b37283916b5
lkdev <lucas.kano@outlook.com>
2022-05-08 09:13:28 -0300

    model correction + migration

--- core/models.py
+++ core/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
+class DayProcess(models.Model):
+    date = models.DateField(auto_now=False, blank=False, null=False)
+    
+
+    db_table = 'day_process'
+
 class Transaction(models.Model):
     origin_bank = models.CharField(blank=False, null=False, max_length=100)
     origin_agency = models.CharField(blank=False, null=False, max_length=100)
@@ -10,13 +16,9 @@ class Transaction(models.Model):
     destiny_account = models.CharField(blank=False, null=False, max_length=100)
     transaction_value = models.DecimalField(blank=False, null=False, decimal_places=2, max_digits=20)
     transaction_date_time = models.DateField(auto_now=False, blank=False, null=False)
+    day_process = models.ForeignKey(DayProcess, on_delete=models.CASCADE)
 
     db_table = 'transaction'
 
 
-class DayProcess(models.Model):
-    date = models.DateField(auto_now=False, blank=False, null=False)
-    transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
-
-    db_table = 'day_process'
 
