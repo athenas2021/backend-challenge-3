c1f974a554aff6bfffa033d96bcdea067d89748e
lkdev <lucas.kano@outlook.com>
2022-05-28 15:32:44 -0300

    Edit user

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
      path('', views.users, name='users'),
      path('user/', views.user, name='user'),
-     path('create_user', views.create_user, name='create_user')
+     path('create_user', views.create_user, name='create_user'),
+     path('edit_user', views.edit_user, name='edit_user')
 ]
