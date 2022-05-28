cb39f0d33d77b4eade4a873f52da458bdaa2ea35
lkdev <lucas.kano@outlook.com>
2022-05-28 15:44:53 -0300

    Edit / delete user + Redirect fix

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
      path('', views.users, name='users'),
      path('user/', views.user, name='user'),
      path('create_user', views.create_user, name='create_user'),
-     path('edit_user', views.edit_user, name='edit_user')
+     path('edit_user', views.edit_user, name='edit_user'),
+     path('delete_user', views.delete_user, name='delete_user')
 ]
